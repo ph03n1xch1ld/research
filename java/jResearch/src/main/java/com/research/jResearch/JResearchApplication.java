e459d5b9f12c16109473d2b36b30d5e3f4cfbd5a
Daniela Pointinger <ph03n1xch1ld@gmail.com>
2018-01-24 21:20:53 +0100

    init

---
+++ java/jResearch/src/main/java/com/research/jResearch/JResearchApplication.java
@@ -0,0 +1,12 @@
+package com.research.jResearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JResearchApplication.class, args);
+	}
+}
